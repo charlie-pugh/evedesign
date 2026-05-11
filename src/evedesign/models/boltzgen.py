94f83186d715f269b8a2fde9bc5be9e9da69bac5
Khaoula Belahsen <49695480+khaoulabelahsen@users.noreply.github.com>
2026-05-10 23:22:54 -0400

    add class attributes

--- src/evedesign/models/boltzgen.py
+++ src/evedesign/models/boltzgen.py
@@ -16,4 +16,19 @@ from evedesign.model import BaseModel, Generator
 
 
 class BoltzGenGenerator(BaseModel, Generator):
-    pass
+    available = IMPORT_AVAILABLE
+    name: str = "BoltzGen"
+    citations: list[str] = []
+
+    # core properties
+    requires_target: bool = True
+    requires_fixed_length: bool = False
+    handles_deletions: bool = False
+    handles_insertions: bool = False
+    requires_gpu: bool = True
+    supports_gpu: bool = True
+    supports_gpu_parallel: bool = False
+    supports_cpu_parallel: bool = False
+
+    required_entity_attributes: list[str] | None = []
+    optional_entity_attributes: list[str] | None = []
